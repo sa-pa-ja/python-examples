cbd3e7b83a9c58c8867a7a5ecd0b038b5e9948e4
Saturnino <161881@unsaac.edu.pe>
2025-06-10 18:00:17 -0500

    Will be replaced by CRLF the next time Git touches it

---
+++ main.py
@@ -0,0 +1,11 @@
+from dataclasses import *
+from typing import *
+import datetime 
+import sys
+import platform
+print(sys.version)
+print(sys.version_info)
+print(platform.python_version())
+print(platform.python_version_tuple())
+
+
